ac88ea4be65783bb2b8983d955d01451d86a214f
Johnathan Kastner <johnathan.kastner@gmail.com>
2019-03-21 09:17:28 -0400

    Commit of solution intended to clearly demonstrate scrollview bug.

---
+++ XamFormsScrollviewBug/XamFormsScrollviewBug/XamFormsScrollviewBug/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamFormsScrollviewBug
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      
+    }
+
+    private void MainPage_OnAppearing(object sender, EventArgs e)
+    {
+      for (int x = 0; x < 115; x++)
+      {
+        NumberGrid.Children.Add(new Label()
+        {
+          Text = x.ToString(),
+        });
+      }
+
+    }
+  }
+}
---
+++ XamFormsScrollviewBug/XamFormsScrollviewBug/XamFormsScrollviewBug/PageWithBug1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamFormsScrollviewBug
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageWithBug1 : ContentPage
+	{
+		public PageWithBug1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
